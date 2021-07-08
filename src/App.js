fd3c754af3dce8fecad9038ac39e638c39e7be89
Claudio Sosa <vicomser.claudio@gmail.com>
2021-07-08 20:09:38 -0300

    tarea 3 / componente CartWidget y componente greetings

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import NavBar from  './components/NavBar';
 import SlidePhoto from './components/SlidePhoto';
 import ItemListContainer from './components/ItemListContainer'
 
+const greetings = [ {"greeting":"Buen dia"},
+                    {"greeting":"Buen Tardes"},
+                    {"greeting":"Buen Noches"},
+                    {"greeting":"Bienvenidos"},
+                    {"greeting":"Hasta pronto"},
+                    {"greeting":"Goodbye"}
+                  ];
+
 function App() {
   return (
     <div className="container">
-      <NavBar />
-      <SlidePhoto />
-      <ItemListContainer />
+        <NavBar />
+        <SlidePhoto />
+        <ItemListContainer greetings={greetings}/>
     </div>
   );
 }
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import '../assets/styles/ItemListContainer.css';
 
-const greetings = ["Buen dia", "Buen Tardes", "Buen Noches",  "Bienvenidos", "Hasta pronto", "Goodbye"];
-
-export default function ItemListContainer() {
+export default function ItemListContainer(props) {
     return (
         <div class="itemList">
-            <p>Lista de Saludos</p>
-           <ul id="list-items">
-              { greetings.map( (greeting ) => (
-                   <li key={greeting}>{greeting}</li>
-               ))
-            }
-           </ul>
+            <div className="container">
+                <p>Lista de Saludos</p>
+            <ul id="list-items">
+                { props.greetings.map( (item, index) => (
+                    <li key={index}>{item.greeting}</li>
+                ))
+                }
+            </ul>
+            </div>
         </div>
     )
 }
